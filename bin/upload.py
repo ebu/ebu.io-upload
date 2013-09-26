3cc694dbb4d7a11d2ecec4064e7fbcbff71c00d2
gcmalloc <gcmalloc@gmail.com>
2013-09-26 11:31:50 +0200

    working example

---
+++ bin/upload.py
@@ -0,0 +1,22 @@
+import requests
+import os
+import sys
+
+HOST = "127.0.0.1:8000"
+URL = "http://{}/downloads/upload/".format(HOST)
+
+if len(sys.argv) != 3:
+    print("Usage: {}: token filename".format(sys.argv[0]))
+    exit(1)
+else:
+    if not os.path.exists(sys.argv[2]):
+        sys.stderr.write("file {} does not exist\n".format(sys.argv[2]))
+        exit(2)
+    files = {'file':open(sys.argv[2])}
+    param = {'token':sys.argv[1]}
+    print("uploading to {}".format(URL))
+    resp = requests.post(URL, files=files, data=param)
+    if resp.ok:
+        print("upload done")
+    else:
+        sys.stderr.write("error {} \n".format(resp.content))
